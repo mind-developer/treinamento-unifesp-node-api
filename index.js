7e2482afffe0c88ea5269633f5c0ef69e009e4eb
Bernardo Camargo <bernardopcamargo@gmail.com>
2020-07-26 22:50:21 -0300

    chore: adiciona morgan como dependencia dev

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const express = require('express')
 const app = express()
 const routes = require('./src/Routes')
 const bodyParser = require('body-parser')
-app.use(bodyParser.json())
+const morgan = require('morgan')
 
+app.use(bodyParser.json())
+app.use(morgan('dev'))
 app.use(routes)
 
 app.listen(PORT, () => {
